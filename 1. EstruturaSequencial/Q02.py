4792c46759bab88a72db492df9462ac5df3dd89b
Tao Hansen <tao.santos@somosicev.com>
2020-08-09 10:25:46 -0300

    Questao02

---
+++ 1. EstruturaSequencial/Q02.py
@@ -0,0 +1,12 @@
+'''
+Questao 02
+Faca um Programa que peca um numero e entao mostre a mensagem: O numero informado foi [numero].
+'''
+
+numero = int(input("Digite um numero: "))
+print("O numero informado foi {}.".format(numero))
+
+'''
+Format Specification Mini-Language
+https://docs.python.org/3/library/string.html#format-specification-mini-language
+'''
