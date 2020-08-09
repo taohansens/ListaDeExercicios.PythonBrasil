129b7b8750229cfd1604dc5de5e7db737b644d10
Tao Hansen <tao.santos@somosicev.com>
2020-08-09 10:16:24 -0300

    Questao01

---
+++ 1. EstruturaSequencial/Q01.py
@@ -0,0 +1,12 @@
+'''
+Questão 01
+Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+'''
+
+print("Alo mundo")
+
+
+'''
+Python print() Function
+https://docs.python.org/3/library/functions.html#print
+'''
